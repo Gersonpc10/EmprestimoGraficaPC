14d47f355961572fd7e088b3665d79507ae3db4a
GersonPC <gersonpc10@hotmail.com>
2024-09-07 11:52:18 -0300

    Acrescentando Valor PASSANDO

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ function calcular() {
     const valorEmprestimo = parseFloat(document.getElementById('valor').value);
     const adicional = parseFloat(document.getElementById('adicional').value);
     const resultDiv = document.getElementById('result');
+    const resDiv = document.getElementById('res');
     
     
     if (isNaN(valorEmprestimo) || isNaN(adicional)) {
         resultDiv.innerHTML = '<p>Por favor, insira valores válidos.</p>';
+        resDiv.innerHTML = '<p>Por favor, insira valores válidos.</p>';
         return;
     }
 
-    let resultHTML = '<table><tr><th>Parcela</th><th>Valor Parcela (R$)</th></th><th>Valor Total (R$)</th></tr>';
+    // CALCULO PARA RECEBER O VALOR
+    let resultHTML = `<th>RECEBER o valor de: R$ ${valorEmprestimo.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</th><table><tr><th>Parcela</th><th>Valor Parcela (R$)</th></th><th>Valor Total (R$)</th></tr>`;
     
     listaTaxas.forEach((taxa, index) => {
         const totalParcela = ((valorEmprestimo + adicional )/(1-(taxa / 100)))/(index+1);
@@ -19,9 +22,24 @@ function calcular() {
         resultHTML += `<tr><td>${index + 1}</td><td>${totalParcela.toFixed(2)}</td><td>${total.toFixed(2)}</td></tr>`;
     });
     
+
     resultHTML += '</table>';
     resultDiv.innerHTML = resultHTML;
 
+
+     // CALCULO PASSANDO O VALOR
+
+    let resHTML = `<th>PASSAR o valor de: R$ ${valorEmprestimo.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</th><table><tr><th>Parcela</th><th>Valor Parcela (R$)</th><th>Valor Total (R$)</th></tr>`;
+
+    listaTaxas.forEach((taxa, index) => {
+        const totalParcela = ((valorEmprestimo))/(index+1);
+        const total = ((valorEmprestimo)*(1-(taxa / 100)) - adicional);
+        resHTML += `<tr><td>${index + 1}</td><td>${totalParcela.toFixed(2)}</td><td>${total.toFixed(2)}</td></tr>`;
+    });
+
+   
+    resHTML += '</table>';
+    resDiv.innerHTML = resHTML;
 
 
 
