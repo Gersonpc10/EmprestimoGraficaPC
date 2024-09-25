d2fdfff979f512bccb1ef4cae80f4279b587978f
GersonPC <gersonpc10@hotmail.com>
2024-09-25 20:18:15 -0300

    Alterado input para um promp na taxa adicional

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+
+
+/* o bloco abaixoi foi removido do html para ser susbtituido pelo prompt
+<label for="adicional">Adicional:</label>
+<input type="number" id="adicional" required>*/
+
+
 const listaTaxas = [3.74, 6.14, 6.89, 7.61, 8.35, 9.06, 9.89, 10.59, 11.29, 11.98, 12.67, 13.35];
 
 function calcular() {
     const valorEmprestimo = parseFloat(document.getElementById('valor').value);
-    const adicional = parseFloat(document.getElementById('adicional').value);
+//  const adicional = parseFloat(document.getElementById('adicional').value);  retirado para subtituir por um alert
     const resultDiv = document.getElementById('result');
     const resDiv = document.getElementById('res');
     
     
+    // Prompt for additional value
+    const adicional = parseFloat(prompt("Digite o valor adicional (R$):"));
+
+
+
+
     if (isNaN(valorEmprestimo) || isNaN(adicional)) {
         resultDiv.innerHTML = '<p>Por favor, insira valores válidos.</p>';
         resDiv.innerHTML = '<p>Por favor, insira valores válidos.</p>';
         return;
     }
 
+    
+    
     // CALCULO PARA RECEBER O VALOR
     let resultHTML = `<th>RECEBER o valor de: R$ ${valorEmprestimo.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</th><table><tr><th>Parcela</th><th>Valor Parcela (R$)</th></th><th>Valor Total (R$)</th></tr>`;
     
